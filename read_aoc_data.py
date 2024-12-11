4680634407b209c687935107d3e29b1705ab4e2c
Rud Faden <rudfaden@gmail.com>
2024-12-11 10:04:18 +0100

    added formatting

--- read_aoc_data.py
+++ read_aoc_data.py
@@ -1,9 +1,16 @@
 import os
+import sys
 
 
 def read_aoc_data():
-    script_dir = os.path.dirname(os.path.realpath(__file__))
+    # Get the directory of the main executing script
+    script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+    # Construct the path to 'input.txt' assuming it is in the same directory
     input_path = os.path.join(script_dir, 'input.txt')
+
+    # Open and read the file
     with open(input_path, "r") as file:
         data = file.read()
+
     return data
